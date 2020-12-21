b3cc8d8576232d8ee5da0d1fb6ead16b6c6c568b
Wojciech Zieliński <wojciechzielinski91@tlen.pl>
2020-12-21 17:39:59 +0100

    added fluid small navbar

--- index.js
+++ index.js
@@ -2,17 +2,11 @@ const bigNavBar = $("#nav-big");
 const smallNavBar = $("#nav-small");
 window.onscroll = ()=>{
     scrollValue = $(window).scrollTop();
-    console.log(scrollValue);
-    console.log(bigNavBar.height());
     if(scrollValue > bigNavBar.height()){
-       // bigNavBar.attr("style","display:none");
         smallNavBar.attr("style","display:block");
-        //window.scrollTo(0,51);
     }
     else{
-        //bigNavBar.attr("style","display:static");
-        smallNavBar.attr("style","display:none");
-        //window.scrollTo(0,0);
+        smallNavBar.attr("style","display:none"); 
     }
     
 }
