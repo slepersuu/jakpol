c097cdc5908fc758dff25fc4475705ac45566543
Wojciech Zieliński <wojciechzielinski91@tlen.pl>
2021-01-25 11:38:35 +0100

    services changed to grid

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const bigNavBar = $("#nav-big");
 const smallNavBar = $("#nav-small");
-const aboutDesc = $(".about-desc");
-let height = 0;
-$(".about-desc p").map(function(){
-    height+=$(this).height();
-    console.log(height);
-})
-$(".services-col").map(function(){
-    $(this).height(height);
-})
+// const aboutDesc = $(".about-desc");
+// let height = 0;
+// $(".about-desc p").map(function(){
+//     height+=$(this).height();
+//     console.log(height);
+// })
+// $(".services-col").map(function(){
+//     $(this).height(height);
+// })
+// $(document).ready(function () {
+//     if($(window).scrollTop() > bigNavBar.height()){
+//         smallNavBar.attr("style","display:block");
+//     }
+// });
+
+
 window.onscroll = ()=>{
     if($(window).scrollTop() > bigNavBar.height()){
         smallNavBar.attr("style","display:block");
@@ -18,9 +25,4 @@ window.onscroll = ()=>{
     }
     
 }
-$(document).ready(function () {
-    if($(window).scrollTop() > bigNavBar.height()){
-        smallNavBar.attr("style","display:block");
-    }
-});
 
